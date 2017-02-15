6ae7d4ac45d1c82a39d608a4fbbb83d02d342afc
klemensl <klemensl@gmail.com>
2017-02-15 22:26:44 +0100

    Implemented "Alexa smart home" functionality

--- Homematic-AlexaCallback/__init__.py
+++ Homematic-AlexaCallback/__init__.py
@@ -23,31 +23,63 @@ class CCUCallbackHandler(BaseHTTPRequestHandler):
 
     #Handler for the POST requests
     def do_POST(self):
-        jsonResponse = json.loads('{ "version": "1.0", "response": { "outputSpeech": { "type": "PlainText", "text": "PLACEHOLDER" }, "card": { "type": "Simple", "title": "PLACEHOLDER" }, "shouldEndSession": true } }')
 
         contentLength = int(self.headers.get('content-length'))
         content = self.rfile.read(contentLength)
 
         jsonContent = json.loads(content)
-        intent = jsonContent["request"]["intent"]
-        print "Intent: ", intent["name"]
 
-        if intent["name"] == "Licht_Schalten":
-            zimmer = intent["slots"]["Zimmer"]["value"]
-            aktion = intent["slots"]["Aktion"]["value"]
+        if jsonContent["header"] is not None: #Smarthome Skill
+            print "Smarthome Skill..."
+            print content
 
-            #print zimmer, aktion
-            eventPayload = [zimmer, aktion]
-            eg.TriggerEvent(intent["name"], eventPayload, "Alexa")
+            if jsonContent['header']['namespace'] == 'Alexa.ConnectedHome.Discovery':
+                #payload = { "discoveredAppliances":[ { "applianceId":"device001", "manufacturerName":"yourManufacturerName", "modelName":"model 01", "version":"your software version number here.", "friendlyName":"Smart Home Virtual Device", "friendlyDescription":"", "isReachable":True, "actions":[ "turnOn", "turnOff" ], "additionalApplianceDetails":{ "extraDetail1":"optionalDetailForSkillAdapterToReferenceThisDevice", "extraDetail2":"There can be multiple entries", "extraDetail3":"but they should only be used for reference purposes.", "extraDetail4":"This is not a suitable place to maintain current device state" } } ] }
+                applianceKueche =  { "applianceId":"KEQ0926487:1", "manufacturerName":"Homematic", "modelName":"Aktor1", "version":"1.0", "friendlyName":"Kueche", "friendlyDescription":"Kueche", "isReachable":True, "actions":[ "turnOn", "turnOff" ], "additionalApplianceDetails":{} }
+                applianceKuecheAmbient =  { "applianceId":"KEQ0926487:2", "manufacturerName":"Homematic", "modelName":"Aktor2", "version":"1.0", "friendlyName":"Kuecheambient", "friendlyDescription":"Kuecheambient", "isReachable":True, "actions":[ "turnOn", "turnOff" ], "additionalApplianceDetails":{} }
+                applianceStiege =  { "applianceId":"LEQ0236800:1", "manufacturerName":"Homematic", "modelName":"Aktor3", "version":"1.0", "friendlyName":"Stiege", "friendlyDescription":"Stiege", "isReachable":True, "actions":[ "turnOn", "turnOff" ], "additionalApplianceDetails":{} }
+                applianceGang =  { "applianceId":"LEQ0236800:2", "manufacturerName":"Homematic", "modelName":"Aktor4", "version":"1.0", "friendlyName":"Gang", "friendlyDescription":"Gang", "isReachable":True, "actions":[ "turnOn", "turnOff" ], "additionalApplianceDetails":{} }
+                applianceEsszimmer =  { "applianceId":"LEQ0883366:1", "manufacturerName":"Homematic", "modelName":"Aktor5", "version":"1.0", "friendlyName":"Esszimmer", "friendlyDescription":"Esszimmer", "isReachable":True, "actions":[ "turnOn", "turnOff" ], "additionalApplianceDetails":{} }
+                #appliance =  { "applianceId":"KEQ0926487:1", "manufacturerName":"Homematic", "modelName":"Aktor", "version":"1.0", "friendlyName":"Kueche", "friendlyDescription":"", "isReachable":True, "actions":[ "turnOn", "turnOff" ], "additionalApplianceDetails":{} }
+                #appliance =  { "applianceId":"KEQ0926487:1", "manufacturerName":"Homematic", "modelName":"Aktor", "version":"1.0", "friendlyName":"Kueche", "friendlyDescription":"", "isReachable":True, "actions":[ "turnOn", "turnOff" ], "additionalApplianceDetails":{} }
+                #appliance =  { "applianceId":"KEQ0926487:1", "manufacturerName":"Homematic", "modelName":"Aktor", "version":"1.0", "friendlyName":"Kueche", "friendlyDescription":"", "isReachable":True, "actions":[ "turnOn", "turnOff" ], "additionalApplianceDetails":{} }
+                #appliance =  { "applianceId":"KEQ0926487:1", "manufacturerName":"Homematic", "modelName":"Aktor", "version":"1.0", "friendlyName":"Kueche", "friendlyDescription":"", "isReachable":True, "actions":[ "turnOn", "turnOff" ], "additionalApplianceDetails":{} }
+                #appliance =  { "applianceId":"KEQ0926487:1", "manufacturerName":"Homematic", "modelName":"Aktor", "version":"1.0", "friendlyName":"Kueche", "friendlyDescription":"", "isReachable":True, "actions":[ "turnOn", "turnOff" ], "additionalApplianceDetails":{} }
 
-            #responseMessage = "Das Licht im {0} ist jetzt {1}.".format(zimmer, aktion)
-            responseMessage = "Klar doch!"
-            jsonResponse["response"]["outputSpeech"]["text"] = responseMessage
-            jsonResponse["response"]["card"]["title"] = responseMessage
-            response = json.dumps(jsonResponse)
-        else:
-            response = '{ "version": "1.0", "response": { "outputSpeech": { "type": "PlainText", "text": "Die Temperatur im Schlafzimmer ist 22 Grad." }, "card": { "type": "Simple", "title": "Homematic A87", "content": "Die Temperatur im Schlafzimmer ist 22 Grad." }, "shouldEndSession": true } }'
+                payload = { "discoveredAppliances":[applianceKueche, applianceKuecheAmbient, applianceStiege, applianceGang, applianceEsszimmer] }
+                response = json.dumps(payload)
+            elif jsonContent['header']['namespace'] == 'Alexa.ConnectedHome.Control':
+                device_id = jsonContent['payload']['appliance']['applianceId']
+                aktion = jsonContent['header']['name'] #== 'TurnOnRequest':
+                eventPayload = [device_id, aktion]
 
+                eg.TriggerEvent("Licht_Schalten", eventPayload, "Alexa")
+                #message_id = event['header']['messageId']
+                payload = {}
+                response = json.dumps(payload)
+        elif jsonContent["request"] is not None: #Custom Skill
+            print "Custom Skill..."
+
+            intent = jsonContent["request"]["intent"]
+            print "Intent: ", intent["name"]
+
+            if intent["name"] == "Licht_Schalten":
+                zimmer = intent["slots"]["Zimmer"]["value"]
+                aktion = intent["slots"]["Aktion"]["value"]
+
+                #print zimmer, aktion
+                eventPayload = [zimmer, aktion]
+                eg.TriggerEvent(intent["name"], eventPayload, "Alexa")
+
+                responseMessage = "Klar doch!"
+                jsonResponse = json.loads('{ "version": "1.0", "response": { "outputSpeech": { "type": "PlainText", "text": "PLACEHOLDER" }, "card": { "type": "Simple", "title": "PLACEHOLDER" }, "shouldEndSession": true } }')
+                jsonResponse["response"]["outputSpeech"]["text"] = responseMessage
+                jsonResponse["response"]["card"]["title"] = responseMessage
+                response = json.dumps(jsonResponse)
+            else:
+                response = '{ "version": "1.0", "response": { "outputSpeech": { "type": "PlainText", "text": "Die Temperatur im Schlafzimmer ist 22 Grad." }, "card": { "type": "Simple", "title": "Homematic A87", "content": "Die Temperatur im Schlafzimmer ist 22 Grad." }, "shouldEndSession": true } }'
+
+        print response
         self.send_response(200)
         self.send_header("Content-type", 'application/json; charset=UTF-8')
         self.send_header("Content-Length", len(response))
@@ -113,3 +145,42 @@ class HMAlexaCallback(eg.PluginBase):
 
     def __close__(self):
         print "Action 'close'"
+
+
+
+'''
+{
+payload = {
+            "discoveredAppliances":[
+                {
+                    "applianceId":"KEQ0926487:1",
+                    "manufacturerName":"Homematic",
+                    "modelName":"Aktor",
+                    "version":"1.0",
+                    "friendlyName":"Kueche",
+                    "friendlyDescription":"",
+                    "isReachable":True,
+                    "actions":[
+                        "turnOn",
+                        "turnOff"
+                    ],
+                    "additionalApplianceDetails":{}
+                }
+            ]
+        }
+
+'''
+
+'''
+header = {
+        "namespace":"Alexa.ConnectedHome.Control",
+        "name":"TurnOnConfirmation",
+        "payloadVersion":"2",
+        "messageId": message_id
+        }
+return { 'header': header, 'payload': payload }
+'''
+
+'''
+{"header": {"payloadVersion": "2", "namespace": "Alexa.ConnectedHome.Discovery", "name": "DiscoverAppliancesRequest"}, "payload": {"accessToken": "someaccesstoken"}}
+'''
